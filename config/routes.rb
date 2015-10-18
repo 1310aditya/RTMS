7d3c9cb531cdb4605b311a70fca2f017293ce358
Aditya Agrawal <agrawal.aditya@outlook.com>
2015-10-18 22:24:20 +0530

    Devise Users added w/ errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'home/index'
@@ -8,7 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
